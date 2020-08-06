b3e087af5c9c1e411485dee348fba445d118c604
super2025 <2110084016@qq.com>
2020-08-06 16:46:47 +0800

    把本地工程添加到git

--- git-test-project/src/com/itheima/git/FirstClass.java
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -10,6 +10,13 @@ public class FirstClass {
         System.out.println("hello git project");
         System.out.println("hello git project");
     }
+
+    public void test (){
+        int i= 0;
+        i += 10;
+    }
+
+
 }
 
 
